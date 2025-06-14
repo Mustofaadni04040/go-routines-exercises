dc3d9874ba9b5bb14e7bbdbe8ec8906ca8b334cd
mustofa adny <adniadni04@gmail.com>
2025-06-14 13:48:43 +0700

    running 100000 go routine to prove goroutine speed

--- goroutine_test.go
+++ goroutine_test.go
@@ -16,3 +16,15 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(1 * time.Second) // seolah-olah synchronous : menunggu sampai selesai semua baru func test berhenti
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
